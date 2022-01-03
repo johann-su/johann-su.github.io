60ad2224207f78c6fe4c3323c5b55a216793fbbb
johann-su <johann123.js@gmail.com>
2022-01-03 19:25:42 +0100

    changed html name

--- star_generator.js
+++ star_generator.js
@@ -3,15 +3,28 @@ let svgns = "http://www.w3.org/2000/svg"
 let parendNode = document.getElementById('stars');
 
 let i = 0
-while (i < randomNumberBetween(5, 20)) {
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+console.log(urlParams.get("amount"));
+const startAmount =  urlParams.get("amount") ? urlParams.get("amount"): randomNumberBetween(5,20);
+const startAmount_el = document.getElementById("stars_count");
+startAmount_el.innerHTML="Stars: " + startAmount.toString();
+const slider = document.getElementById("amount");
+slider.value = urlParams.get("amount") ? urlParams.get("amount"): startAmount;
+
+while (i < startAmount) {
   i = i+1
 
   const starNode = document.createElementNS(svgns, 'polygon')
+  
   const position = { x: randomNumberBetween(10, 1920), y: randomNumberBetween(10,600) }
+  
   const scale = randomNumberBetween(1,100)/100;
   starNode.classList.add('star')
   
   starNode.setAttribute('fill', '#ffd700')
+  // starNode.setAttribute('filter', 'url(#star_glow)')
   starNode.setAttribute('points', generateShape(position, scale))
 
   starNode.innerHTML = `<animateTransform attributeName="transform" type="rotate" from="0 ${position.x + 100} ${position.y + 100}" to="360 ${position.x + 100} ${position.y + 100}" dur="4s" repeatCount="indefinite" />`
@@ -39,4 +52,12 @@ function generateShape(position, scale) {
 
 function randomNumberBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function reload(){
+ 
+  let url = window.location.href;
+  url = url.split("?amount=")[0]
+  console.log('url: ' + url)
+  window.location.href = url + "?amount=" + slider.value.toString();
 }
